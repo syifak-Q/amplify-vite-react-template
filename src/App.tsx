0fbbd950bbe9db93aa989283ea84d40924a3fc56
syifaq57 <syifaq57@gmail.com>
2024-10-10 10:30:39 +0700

    trtry auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
-import { useAuthenticator } from '@aws-amplify/ui-react';
+import { Authenticator } from '@aws-amplify/ui-react';
 
 const client = generateClient<Schema>();
 
 function App() {
 
-  const { signOut } = useAuthenticator();
+  // const { signOut } = useAuthenticator();
 
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
@@ -27,27 +27,33 @@ function App() {
   }
 
   return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li 
-            key={todo.id} 
-            onClick={() => deleteTodo(todo.id)}>
-              {todo.content}
-          </li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
-      <button onClick={signOut}>Sign out</button>
-    </main>
+    <Authenticator>
+      {({signOut, user}) => (
+        <main>
+        <h1>My todos</h1>
+        <button onClick={createTodo}>+ new</button>
+        <ul>
+          {todos.map((todo) => (
+            <li 
+              key={todo.id} 
+              onClick={() => deleteTodo(todo.id)}>
+                {todo.content}
+            </li>
+          ))}
+        </ul>
+        <div>
+          🥳 App successfully hosted. Try creating a new todo.
+          <br />
+          <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
+            Review next step of this tutorial.
+          </a>
+        </div>
+        <h1>{user?.signInDetails?.loginId}'s</h1>
+        <button onClick={signOut}>Sign out</button>
+      </main>
+      )}
+    </Authenticator>
+    
   );
 }
 
